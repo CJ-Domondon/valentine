9118ae163accd69ab5c9518c12c3a32d49f4ef46
CJ-Domondon <155409599+CJ-Domondon@users.noreply.github.com>
2024-02-04 15:13:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
   
       // Update text content, show message, and change image source to "dance.gif"
       const yesMessage = document.getElementById("question");
-      yesMessage.textContent = "LESGOOO see you on February 14 after our class Love😘😘";
+      yesMessage.textContent = "Yeeeey see you on February 14 after our class Love😘😘";
       yesMessage.style.display = "block";
       yesMessage.style.fontStyle = "normal";
       document.getElementsByClassName("images")[0].src = "blush.gif";
